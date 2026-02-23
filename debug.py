73b7062b3e9af53465999ed611562a4fde1ede08
Licht005 <lucaskpatah8@gmail.com>
2026-02-23 03:08:06 +0000

    removed unsused files

--- debug.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import PDFPlumberLoader
-
-loader = PDFPlumberLoader("Doc/Ghana Constitution.pdf")
-docs = loader.load()
-
-full_text = " ".join([d.page_content for d in docs])
-idx = full_text.find("6")
-print(repr(full_text[idx:idx+600]))
